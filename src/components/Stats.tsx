6160f1611a1c2c716521d481eb85214c3fc51147
bekturb <bekkgboy2@gmail.com>
2024-10-31 17:11:08 +0600

    finished services component

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -35,13 +35,13 @@ const Stats = () => {
                 {stats.map((item, index) => (
                     <div
                         className='flex-1 flex gap-4 items-center justify-center 
-                        xl:justify-start' 
+                        xl:justify-start'
                         key={index}>
                         <CountUp 
                             end={item.num}
                             duration={5}
                             delay={2}
-                            className='text-4xl xl:text-6xl font-semibold'
+                            className='text-4xl xl:text-6xl font-extrabold'
                         />
                         <p className={`${item.text.length < 15 ? "max-w-[100px]" : "max-w-[150px]"} leading-snug text-white/80`}>
                             {item.text}
